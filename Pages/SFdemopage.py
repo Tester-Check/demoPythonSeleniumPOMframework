7fdd2fed6438eceb76f2f1b6f94ad755bcd8d4d9
sooraj <soorajh4@gmail.com>
2016-03-08 23:13:41 +0530

    commit form second mac

--- Pages/SFdemopage.py
+++ Pages/SFdemopage.py
@@ -1,9 +1,20 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from common_actions.Actions import CommonActions
+
 
 
 class SFdemoPage():
 
-    def _init(self, browser):
-        self.browser = browser
+    def __init__(self, browser):
+        self.browser = browser#=webdriver.Firefox()
+
+    new_window_button = (By.CSS_SELECTOR, "#button1")
+    menu_button = (By.CSS_SELECTOR, "#mobile-menu")
+
 
     def switch_actions(self):
-        self.
+        CommonActions(self.browser).click(SFdemoPage.new_window_button)
+        CommonActions(self.browser).switch_to_latest_window()
+        CommonActions(self.browser).click(SFdemoPage.menu_button)
+
