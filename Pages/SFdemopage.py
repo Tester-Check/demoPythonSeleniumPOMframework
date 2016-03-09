08c479adf04ae1cde3232210ea1600915036eec0
sooraj <soorajh4@gmail.com>
2016-03-09 08:37:52 +0530

    commit3.2 form second mac2

--- Pages/SFdemopage.py
+++ Pages/SFdemopage.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from common_actions.Actions import CommonActions
+import time
 
 
 
@@ -11,11 +12,29 @@ class SFdemoPage():
 
     new_window_button = (By.CSS_SELECTOR, "#button1")
     menu_button = (By.CSS_SELECTOR, "#mobile-menu")
+    new_message_window = (By.XPATH, "//button[@onclick='newMsgWin()']")
+    message_on_new_window = (By.XPATH, "//*[contains(text()='message']")
+    new_browser_tab = (By.XPATH, "//button[@onclick='newBrwTab()']")
 
 
     def switch_actions(self):
-        """perform switch actions pop up window/modal/alert etc"
-        CommonActions(self.browser).click(SFdemoPage.new_window_button)
+        """perform switch actions pop up window/modal/alert etc"""
+        CommonActions(self.browser).click(self.new_window_button)
         CommonActions(self.browser).switch_to_latest_window()
-        CommonActions(self.browser).click(SFdemoPage.menu_button)
+        CommonActions(self.browser).click(self.menu_button)
+        self.browser.close()
+        CommonActions(self.browser).switch_to_latest_window()
+
+        CommonActions(self.browser).click(self.new_message_window)
+        CommonActions(self.browser).switch_to_latest_window()
+        self.browser.maximize_window()
+        time.sleep(5)
+        #print(CommonActions(self.browser).get_text(self.message_on_new_window))
+        print(self.browser.page_source)
+        self.browser.close()
+        CommonActions(self.browser).switch_to_latest_window()
+
+        print(self.browser.current_url)
+        print(self.browser.get_cookies())
+
 
